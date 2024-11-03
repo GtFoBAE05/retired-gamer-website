0cfa65341df0d5d811c325977d13a9e1dea5effa
GtFoBAE05 <y.imannuel@gmail.com>
2024-11-03 18:42:04 +0700

    feat: add footer

---
+++ src/components/footer/index.js
@@ -0,0 +1,12 @@
+import style from './footer.module.css'
+export default function Footer() {
+    return (
+        <>
+            <div className={style.footerContainer}>
+                <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.vecteezy.com%2Fsystem%2Fresources%2Fpreviews%2F015%2F820%2F758%2Foriginal%2Fgamer-line-icon-design-free-vector.jpg&f=1&nofb=1&ipt=7fa3a1f568224f50579bc848adba44c3dadceef5aa40a891e378da03f6fcb256&ipo=images" alt=""/>
+                <h3>Copyright © 2024| RETIRED GAMER</h3>
+                <a href=""><img src="https://static-00.iconduck.com/assets.00/itch-io-icon-1024x1024-fev9qpvd.png" alt=""/></a>
+            </div>
+        </>
+    )
+}
